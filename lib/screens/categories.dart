5e2b22e54822bb05dab439ea1a18bcd5ed68fe81
Anagha Yawale <111850322+anaghayawale@users.noreply.github.com>
2023-06-29 22:16:41 -0700

    Finetuning Explicit Animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -76,11 +76,14 @@ class _CategoriesScreenState extends State<CategoriesScreen> with SingleTickerPr
               )
           ],
         ),
-        builder: (context, child) => Padding(
-              padding:
-                  EdgeInsets.only(top: 100 - _animationController.value * 100),
+        builder: (context, child) => SlideTransition(
+              position:
+                  Tween(begin: const Offset(0, 0.3), end: const Offset(0, 0))
+                      .animate(CurvedAnimation(
+                          parent: _animationController,
+                          curve: Curves.easeInOut)),
               child: child,
-            ),
+        )
     );
   }
 }
